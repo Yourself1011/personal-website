b399208440aea0744a0fd884c2aa377d2fa36fac
Daniel Zhang <d.zhang200788@gmail.com>
2024-05-14 22:39:01 -0400

    make images bigger

--- src/lib/imgToAscii.js
+++ src/lib/imgToAscii.js
@@ -4,12 +4,9 @@ const ansi_up = new AnsiUp();
 
 let path = process.argv[2];
 
-imageToAscii(path, { size: { width: 16 }, white_bg: false }, (err, res) => {
+imageToAscii(path, { size: { width: 27 }, white_bg: false }, (err, res) => {
     console.log(res);
     console.log(
-        ansi_up
-            .ansi_to_html(res)
-            .replaceAll("\n", "<br/>\n")
-            .replaceAll("> <", ">&nbsp;<")
+        ansi_up.ansi_to_html(res).replaceAll("\n", "<br/>\n").replaceAll("> <", ">&nbsp;<")
     );
 });
