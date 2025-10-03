a7de20957db1fa5132abece24bc3cb619e92f13a
Daniel Zhang <d.zhang200788@gmail.com>
2025-10-03 15:19:34 -0400

    add opus and obama

--- src/lib/imgToAscii.js
+++ src/lib/imgToAscii.js
@@ -7,6 +7,9 @@ let path = process.argv[2];
 imageToAscii(path, { size: { width: 27 }, white_bg: false }, (err, res) => {
     console.log(res);
     console.log(
-        ansi_up.ansi_to_html(res).replaceAll("\n", "<br/>\n").replaceAll("> <", ">&nbsp;<")
+        ansi_up
+            .ansi_to_html(res)
+            .replaceAll("\n", "<br/>\n")
+            .replaceAll("> <", ">&nbsp;<"),
     );
 });
